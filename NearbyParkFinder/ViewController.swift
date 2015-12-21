d559840261524cc15c22ee53635b4e572b056cf7
SchoonSauce <adamschoonmaker12@gmail.com>
2015-12-21 15:55:00 -0500

    initial commit

---
+++ NearbyParkFinder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NearbyParkFinder
+//
+//  Created by adam on 12/21/15.
+//  Copyright © 2015 Adam Schoonmaker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
